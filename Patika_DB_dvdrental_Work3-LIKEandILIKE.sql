45094ce91ddb6c7ab9740891f060645939581cb3
Kadir GUR <75279588+yourairman@users.noreply.github.com>
2022-10-04 22:36:33 +0300

    Update Patika_DB_dvdrental_Work3-LIKEandILIKE.sql

--- Patika_DB_dvdrental_Work3-LIKEandILIKE.sql
+++ Patika_DB_dvdrental_Work3-LIKEandILIKE.sql
@@ -11,7 +11,7 @@ SELECT * FROM country WHERE country LIKE '%_____n';
 
 --3 film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
 
-SELECT * FROM film WHERE title ILIKE '%t' OR title ILIKE 't%';
+SELECT * FROM film WHERE title ILIKE '%t' OR title ILIKE 't%t%t%t%';
 
 --4 film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 
