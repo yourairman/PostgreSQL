229c1492a3132f5379252c7b5473d7adef9b6afb
Kadir GUR <75279588+yourairman@users.noreply.github.com>
2022-10-05 23:38:12 +0300

    Update Patika_DB_dvdrental_Work5_ORDERBY_LIMIT_OFFSET.sql

--- Patika_DB_dvdrental_Work5_ORDERBY_LIMIT_OFFSET.sql
+++ Patika_DB_dvdrental_Work5_ORDERBY_LIMIT_OFFSET.sql
@@ -3,4 +3,4 @@ SELECT * FROM film WHERE title LIKE '%n' ORDER BY LENGTH DESC LIMIT 5;
 --2 film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
 SELECT * FROM film WHERE title LIKE '%n' ORDER BY LENGTH ASC OFFSET 5 LIMIT 5;
 --3 customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
-SELECT * FROM customer  ORDER BY last_name DESC OFFSET 1 LIMIT 4;
+SELECT * FROM customer  ORDER BY last_name DESC OFFSET 1 LIMIT 3;
